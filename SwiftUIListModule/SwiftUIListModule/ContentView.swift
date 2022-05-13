5f18485d586e4a418154c749625d3e844c278cce
Darya Kuliashova <kuldiand@gmail.com>
2022-05-13 11:04:13 +0200

    Add project

---
+++ SwiftUIListModule/SwiftUIListModule/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIListModule
+//
+//  Created by Darya Kuliashova on 13/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIListModule/SwiftUIListModule/SwiftUIListModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListModuleApp.swift
+//  SwiftUIListModule
+//
+//  Created by Darya Kuliashova on 13/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
